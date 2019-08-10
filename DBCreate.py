2a36be54f3040862b6e5c54f1ae01b37562e7085
Spitford <boyleb@hotmail.com>
2019-08-09 22:34:18 -0400

    DBCreate file added for creating database

---
+++ DBCreate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sqlite3
+
+def create():
+
+    conn = sqlite3.connect('pokemon.db')
+    print ("Opened database successfully")
+
+    conn.execute('''CREATE TABLE MASTER
+                (ID INT PRIMARY KEY     NOT NULL,
+                NAME            TEXT    NOT NULL,
+                TYPE1           TEXT    NOT NULL,
+                TYPE2           TEXT,
+                HP              INT     NOT NULL,
+                ATTACK          INT     NOT NULL,
+                DEFENSE         INT     NOT NULL,
+                SATTACK         INT     NOT NULL,
+                SDEFENSE        INT     NOT NULL,
+                SPEED           INT     NOT NULL,
+                GENDER          REAL    NOT NULL);''')
+
+    print ("Table created successfully")
+
+    conn.close()
+
+if __name__ == '__main__':
+    create()
--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # import libraries
 import sqlite3
 import urllib2
